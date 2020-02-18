f6e1984da2553538545e37f0270a3a29cca1c24a
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-19 07:53:12 +0800

    Load properties files into Spring Boot Cloud condig

--- src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,7 +2,11 @@ package com.in28minutes.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+// You MUST put @EnableConfigServer so the Spring Cloud Configurations can work***
+// Type:
+@EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication {
 
